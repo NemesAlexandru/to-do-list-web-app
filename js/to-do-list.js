bf5f9504844504dcb53a4553659d9adeda7ca582
NemesAlexandru <alexandrunemes1@gmail.com>
2020-02-12 22:32:28 +0200

    finished web

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -7,7 +7,7 @@ window.ToDoList = {
             url: ToDoList.API_URL,
             method: "GET"
         }).done(function (response) {
-            console.log("GET DONE");
+            console.log("GET done");
             console.log(response);
 
             ToDoList.displayItems(JSON.parse(response));
@@ -26,7 +26,7 @@ window.ToDoList = {
         $.ajax({
             url: ToDoList.API_URL,
             method: "POST",
-            //MIME type
+            // MIME type
             contentType: "application/json",
             data: JSON.stringify(requestBody)
         }).done(function () {
@@ -34,32 +34,53 @@ window.ToDoList = {
         })
     },
 
+    markItemDone: function (id, done) {
+        let requestBody = {
+            done: done
+        };
+
+        $.ajax({
+            url: ToDoList.API_URL + "?id=" + id,
+            method: "PUT",
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+            ToDoList.getItems();
+        })
+    },
+
+    deleteItem: function (id) {
+        $.ajax({
+            url: ToDoList.API_URL + "?id=" + id,
+            method: "DELETE"
+        }).done(function () {
+            ToDoList.getItems();
+        })
+    },
+
     displayItems: function (items) {
-
-
-        var tableContent = "";
+        var tableContent = '';
 
         items.forEach(item => tableContent += ToDoList.getItemTableRow(item));
 
         $("#to-do-items tbody").html(tableContent);
-
-        console.log(tableContent);
     },
 
     getItemTableRow: function (item) {
+        // spread (...)
+        var deadline = new Date(...item.deadline).toLocaleDateString("ro");
 
-        // spread
-        var deadline = new Date(...item.deadline).toLocaleDateString("en");
-
-        //ternary operator
+        // ternary operator
         var checkedAttribute = item.done ? "checked" : "";
 
         return `<tr>
             <td>${item.description}</td>
             <td>${deadline}</td>
-            <td><input type="checkbox" class="mark-done" data-id="${item.id}" ${checkedAttribute}/></td>
-            <td><a href="#" class="delete-item"data-id="${item.id}"
-            ><i class="far fa-trash-alt"></i></a> </td>
+            <td><input type="checkbox" class="mark-done" 
+            data-id="${item.id}" ${checkedAttribute}/></td>
+            <td><a href="#" class="delete-item" data-id="${item.id}">
+                <i class="far fa-trash-alt"></i>
+            </a></td>
         </tr>`
     },
 
@@ -69,7 +90,26 @@ window.ToDoList = {
             event.preventDefault();
 
             ToDoList.createItem();
-        })
+        });
+
+        // delegate is necessary because our checkbox is
+        // dynamically injected in the page (not present from the beginning, on page load)
+        $("#to-do-items").delegate(".mark-done", "change", function (event) {
+            event.preventDefault();
+
+            let id = $(this).data("id");
+            let checked = $(this).is(":checked");
+
+            ToDoList.markItemDone(id, checked);
+        });
+
+        $("#to-do-items").delegate(".delete-item", "click", function (event) {
+            event.preventDefault();
+
+            let id = $(this).data("id");
+
+            ToDoList.deleteItem(id);
+        });
     }
 
 };
